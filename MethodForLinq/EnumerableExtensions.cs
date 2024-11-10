cc1ca6090aa2808adad0de35d6010ceb21f868c1
SergDad <dadkof@mail.ru>
2024-11-10 21:40:00 +0500

    Extensions for IEnumerable
    Extensions for IEnumerable MethodForLinq

---
+++ MethodForLinq/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace MethodForLinq
+{
+    public static class EnumerableExtensions
+    {
+        // Дженерик методы расширения для IEnumerable, принимающие значение Х от 1 до 100 процентов
+        // и возвращающий элементы выборки в порядке убывания в количестве, соответсвующем заданному
+        // количеству процентов с округлением количества элементов в большую сторону.
+
+        // Дженерик метод Top, сортирующий коллекцию по умолчанию.
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percentage)
+        {
+            var sortedCollection = collection.OrderByDescending(x => x).ToList();
+            return MyTop<T>(sortedCollection, percentage);
+        }
+
+        // Дженерик перегрузка метода Top c делегетом Func, указывающим поле, по которому проводится сортировка.
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percentage, Func<T, int> selector)
+        {
+            var sortedCollection = collection.OrderByDescending(selector).ToList();
+            return MyTop<T>(sortedCollection, percentage);
+        }
+
+        // Вспомогательный метод для сокращения дублирования кода.
+        public static IEnumerable<T> MyTop<T>(IEnumerable<T> collection, int percentage)
+        {
+            if (percentage < 1 || percentage > 100)
+                throw new ArgumentException("Percentage must be between 1 and 100");
+            int countToTake = (int)Math.Ceiling(collection.Count() * (percentage / 100.0));
+            return collection.Take(countToTake);
+        }
+    }
+}
---
+++ MethodForLinq/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MethodForLinq
+{
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Метод расширения с названием \"Top\" для коллекции IEnumerable");
+            // Пример для int[]
+            Console.WriteLine("\nПроверка для массива int по заданию");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("numbers: " + string.Join(", ", numbers));
+            var topNumbers = numbers.Top(30);
+            Console.WriteLine("  Top 30% numbers: " + string.Join(", ", topNumbers));
+            topNumbers = numbers.Top(20);
+            Console.WriteLine("  Top 20% numbers: " + string.Join(", ", topNumbers));
+
+            Console.WriteLine("\nЕще одна проверка для массива int");
+            numbers = [ 19, 20, 13, 4, 15, 26, 17, 8, 9 ];
+            Console.WriteLine("numbers: " + string.Join(", ", numbers));
+            topNumbers = numbers.Top(30);
+            Console.WriteLine("  Top 30% numbers: " + string.Join(", ", topNumbers));
+            topNumbers = numbers.Top(20);
+            Console.WriteLine("  Top 20% numbers: " + string.Join(", ", topNumbers));
+
+            // Пример для List<Person>
+            Console.WriteLine("\nПроверка для List<Person>");
+            var people = new List<Person>
+            {
+                new() { Name = "Sergey", Age = 55 },
+                new() { Name = "Boris", Age = 30 },
+                new() { Name = "Pavel", Age = 35 },
+                new() { Name = "Aleksey", Age = 40 },
+                new() { Name = "Dmitriy", Age = 45 },
+                new() { Name = "Mikhail", Age = 25 }
+            };
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+            var topPeopleByAge = people.Top(40, person => person.Age);
+            Console.WriteLine("Top 40% people by age (.Top(40, person => person.Age):");
+            foreach (var person in topPeopleByAge)
+            {
+                Console.WriteLine(person);
+            }
+
+            topPeopleByAge = people.Top(60, person => person.Age);
+            Console.WriteLine("\nTop 60% people by age (.Top(40, person => person.Age):");
+            foreach (var person in topPeopleByAge)
+            {
+                Console.WriteLine(person);
+            }
+
+
+            // Пример для массива int
+            Console.WriteLine("\nПроверка для Random массива int");
+            var array = new int[30];
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+            };
+            Console.WriteLine("array: " + string.Join(", ", array));
+            var topArray = array.Top(20);
+            Console.WriteLine("  Top 20% array: " + string.Join(", ", topArray));
+
+            try
+            {
+                Console.WriteLine("\nПроверка для .Top(0)");
+                topArray = array.Top(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"    Ошибка: {ex.Message}");
+            }
+            try
+            {
+                Console.WriteLine("\nПроверка для .Top(120)");
+                topArray = array.Top(120);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"    Ошибка: {ex.Message}");
+            }
+        }
+    }
+}

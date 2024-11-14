fdd88f619466d4e1b9a78751b3038a77b1c4aeb3
SergDad <dadkof@mail.ru>
2024-11-15 00:08:48 +0500

    Dictionary
    Реализация словаря

---
+++ OtusDictionary/OtusDictionary.cs
@@ -0,0 +1,102 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace OtusDictionary
+{
+    public class OtusDictionary
+    {
+        #region Описания и конструктор
+        private const int InitialSize = 32;
+        private int _size;
+        private int _count;
+        private int[] _keys;
+        private string[] _values;
+
+        public OtusDictionary()
+        {
+            _size = InitialSize;
+            _keys = new int[InitialSize];
+            _values = new string[InitialSize];
+            _count = 0;
+        }
+        #endregion
+        #region  Метод добавления элемента
+        public void Add(int key, string? value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value), "Значение элемента не может быть null");
+
+            int index = HashFunction(key);
+            if (_keys[index] == key && _values[index] != null)
+                throw new ArgumentException($"Элемент с ключем = {key} уже есть");  //, nameof(key));
+            while (_values[index] != null)
+            {
+                // Если index уже занят другим key, увеличиваем хранилище в два раза
+                Resize();
+                index = HashFunction(key);
+            }
+            _keys[index] = key;
+            _values[index] = value;
+            _count++;
+        }
+        #endregion
+        #region  Метод получения элемента по ключу
+        public string? Get(int key)
+        {
+            int index = HashFunction(key);
+            if (_keys[index] == key)
+            {
+                return _values[index];
+            }
+            return null; // Элемент не найден
+        }
+        #endregion
+        #region  Индексатор для доступа по ключу
+        public string? this[int key]
+        {
+            get => Get(key);
+            set => Add(key, value);
+        }
+        #endregion
+        #region Хеш-функция
+        private int HashFunction(int key)
+        {
+            if (key > -1) return key * 2 % _size;
+            return (key * (-2) - 1) % _size;
+        }
+        private int HashFunction(int key, int size)
+        {
+            if (key > -1) return key * 2 % size;
+            return (key * (-2) - 1) % size;
+        }
+        #endregion
+        #region Метод для увеличения размера массива
+        private void Resize()
+        {
+            int newSize = _size * 2;
+            // Console.WriteLine($"Resize to {newSize}");
+            int[] newKeys = new int[newSize];
+            string[] newValues = new string[newSize];
+            int newIndex;
+
+            for (int i = 0; i < _size; i++)
+            {
+                if (_values[i] != null)
+                {
+                    newIndex = HashFunction(_keys[i], newSize);
+                    newKeys[newIndex] = _keys[i];
+                    newValues[newIndex] = _values[i];
+                }
+            }
+            _size = newSize;
+            _keys = newKeys;
+            _values = newValues;
+        }
+        #endregion
+
+
+    }
+}
---
+++ OtusDictionary/Program.cs
@@ -0,0 +1,82 @@
+﻿# region Условие
+using System;
+namespace OtusDictionary
+//  Реализуем свой собственный словарь
+
+//  Цель:
+//  Реализуйте класс OtusDictionary, который позволяет оперировать int-овыми значениями в качестве ключей
+//  и строками в качестве значений. Для добавления используйте метод void Add(int key, string value),
+//  а для получения элементов - string Get(int key).
+//  Внутреннее хранилище реализуйте через массив. При нахождении коллизий, создавайте новый массив
+//  в два раза больше и не забывайте пересчитать хеши для всех уже вставленных элементов.
+//  Метод GetHashCode использовать не нужно и массив/список объектов по одному адресу создавать тоже не нужно
+//  (только один объект по одному индексу). Словарь не должен давать сохранить null в качестве строки,
+//  соответственно, проверять заполнен элемент или нет можно сравнивая строку с null.
+//
+//  Описание/Пошаговая инструкция выполнения домашнего задания:
+//  1. Реализуйте метод Add с неизменяемым массивом размером 32 элемента(исключение, если уже занято место).
+//  2. Реализуйте метод Get.
+//  3. Реализуйте увеличение массива в два раза при нахождении коллизий.
+//  4. Убедитесь, что класс работает без ошибок (например, Get несуществующего элемента) не бросает исключений,
+//  помимо заданных вами.Если это не так, то доработайте.
+//  5. Добавьте к классу возможность работы с индексатором.
+# endregion
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Проверка работы класса OtusDictionary");
+            OtusDictionary? dict = new OtusDictionary();
+            Random rnd = new Random();
+            int key;
+            string value;
+            Item item = new();
+            List<Item> items = new();
+            for (int i = 0; i < 200; i++)
+            {
+                try
+                {
+                    key = rnd.Next(-400, 400);
+                    value = $"Text for Key {key}";
+                    dict.Add(key, value);
+                    item.S(key, value);
+                    items.Add(item);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Обработка еxception {e.Message}");
+                }
+            }
+            string? dictFromKey;  // Содержимое словаря dict по ключу key
+            bool enableTest = true;
+            foreach (Item item1 in items)
+            {
+                key = item1.key;
+                dictFromKey = dict.Get(key);
+                if (item1.value != dictFromKey)
+                {
+                    Console.WriteLine($"Для ключа {key} значение не найдено или неверное: {dictFromKey ?? "NULL"}");
+                    enableTest = false;
+                }
+                //else
+                //    Console.WriteLine($"ключ {key}, {dictFromKey ?? "NULL"}");
+            }
+            if (!enableTest)
+                Console.WriteLine("Класс OtusDictionary тест не прошел");
+            else
+                Console.WriteLine("Класс OtusDictionary тест прошел");
+        }
+        struct Item
+        {
+            internal int key;
+            internal string? value;
+            public void S(int keyS, string valueS)
+            {
+                key = keyS;
+                value = valueS;
+            }
+        };
+    };
+
+}
